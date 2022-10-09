6f20906b2afd994104955756de57eb456651c7b7
belljun3395 <195850@jnu.ac.kr>
2022-10-09 14:38:02 +0900

    feat : 초기세팅

---
+++ src/main/java/com/econv/mentoring24Th/Mentoring24ThApplication.java
@@ -0,0 +1,13 @@
+package com.econv.mentoring24Th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mentoring24ThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mentoring24ThApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/econv/mentoring24Th/Mentoring24ThApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econv.mentoring24Th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mentoring24ThApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
